523fe488ebf43a78891d5ffa6195b87b53c93362
Nguyen Thanh Trieu <trieunguyenthanh@admicro.vn>
2020-09-10 23:03:01 +0700

    push code

---
+++ src/app/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+// tao component
+class MyApp extends React.Component {
+  render(){
+    return(
+      <React.Fragment>
+        <h1 style={{color: 'red'}}>Hello word</h1>
+      </React.Fragment>
+    );
+  }
+}
+export default MyApp;
