ad8ff0cb5b3b0245af38944f975ee434f92c5cab
Nguyen Thanh Trieu <trieunguyenthanh@admicro.vn>
2020-09-17 23:23:29 +0700

    update code

--- src/counter/Counter.js
+++ src/counter/Counter.js
@@ -5,21 +5,47 @@ import './counter.css';
 
 class Counter extends Component {
 
+  constructor(props){
+    super(props);// de co the su dung dc tat ca cac props cua class cha truyen xuong
+    // tao ra 1 state cho component nao do
+    this.state = {
+      count: 0
+    }
+    this.viewIncrement = this.viewIncrement.bind(this);
+  }
+
   // xu ly su kien trong reactjs
   // dinh nghia 1 ham xu ly event (su kien)
-  changeCounter = () => {
-    alert('ban vua click vao toi');
+  changeIncrementCounter = () => {
+    // cap nhat lai state o day
+    //this.setState({count: this.state.count + 1});
+    this.setState((state) => {
+      return {...state, count: state.count + 1}
+    });
+  }
+  changeDecrementCounter = () => {
+    this.setState((state) => {
+      return {count: state.count - 1}
+    });
+  }
+
+  viewIncrement() {
+    this.changeIncrementCounter();
+    this.changeIncrementCounter();
+    this.changeIncrementCounter();
   }
 
   render() {
     return(
       <>
         <div className="container">
-          <Result result={0} />
+          <Result result={this.state.count} />
           <ButtonCounter
-            click={this.changeCounter}
+            click={this.viewIncrement}
           >+</ButtonCounter>
-          <ButtonCounter>-</ButtonCounter>
+          <ButtonCounter
+            click={this.changeDecrementCounter}
+          >-</ButtonCounter>
         </div>
       </>
     )
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './counter/Counter';
+import App from './checkyear/index';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
